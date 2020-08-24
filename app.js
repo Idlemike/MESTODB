6cf4770b6b05e4be47873aed844d68483c1b064c
idlemike <idlemike@yandex.ru>
2020-08-24 14:58:49 +0300

    helmet

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+
 const AppError = require('./app_server/utils/appError');
 const globalErrorHandler = require('./app_server/controllers/errorController');
 const userRouter = require('./app_server/routes/userRoutes');
@@ -9,15 +12,27 @@ const { createUser, login } = require('./app_server/controllers/authController')
 const app = express();
 
 // 1) MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour!'
+  message: 'Too many requests from this IP, please try again in an hour!',
 });
 
-app.use('/api', limiter);
-app.use(express.json());
+app.use('/', limiter);
 
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   console.log(req.headers);
