389fd7ba6272ad783d971c044d533bf01e5d47b5
idlemike <idlemike@yandex.ru>
2020-08-30 09:35:20 +0300

    auth env corrected

--- app_server/middlewares/auth.js
+++ app_server/middlewares/auth.js
@@ -5,6 +5,8 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const Card = require('../models/cardsModel');
 
+const { JWT_SECRET = 'dev-key' } = process.env;
+
 exports.protect = catchAsync(async (req, res, next) => {
   // 1) Getting token and check of it's there
   let token;
@@ -16,7 +18,7 @@ exports.protect = catchAsync(async (req, res, next) => {
     return next(new AppError('You are not logged in! Please log in to get access.', 401));
   }
   // 2) Verification token
-  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  const decoded = await promisify(jwt.verify)(token, JWT_SECRET);
 
   // 3) Check if user still exists
   const currentUser = await User.findById(decoded.id);
