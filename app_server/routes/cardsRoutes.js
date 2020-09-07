43df68ada98222e5290e65670ba81ecd909967a5
idlemike <idlemike@yandex.ru>
2020-09-07 14:17:52 +0300

    Merge branch 'develop'
    # Conflicts:
    #	package-lock.json

--- app_server/routes/cardsRoutes.js
+++ app_server/routes/cardsRoutes.js
@@ -6,19 +6,9 @@ const {
 const auth = require('../middlewares/auth');
 
 const router = express.Router();
-/*router.route('/:id/likes').put(auth.protect, likeCard);
-
-router.route('/:id/likes').delete(auth.protect, dislikeCard);*/
 
 router.route('/last-5').get(auth.protect, aliasLast, getCards);
 
 router.route('/').get(auth.protect, getCards);
-
-/*router.route('/').post(auth.protect, postCard);
-
-router.route('/:id').delete(auth.protect,
-  auth.restrictTo, deleteCard);
-
-router.route('/:id').get(auth.protect, getCard);*/
 
 module.exports = router;
--- app_server/routes/userRoutes.js
+++ app_server/routes/userRoutes.js
@@ -1,21 +1,11 @@
 const express = require('express');
 const {
   getAllUsers,
-  getUser,
-  patchUser,
-  patchUserAvatar,
-  /*  checkID,*/
 } = require('../controllers/userController');
 const auth = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.route('/').get(auth.protect, getAllUsers);
-
-/*router.route('/:id').get(auth.protect, getUser);
-
-router.route('/me').patch(auth.protect, patchUser);
-
-router.route('/me/avatar').patch(auth.protect, patchUserAvatar);*/
 
 module.exports = router;
